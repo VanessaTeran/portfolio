a1c3ab9530ed40a9657e0cbdcd5ecd37dfb14d52
vanessa.calderon@icbc.com.ar <CV118004@ardp.local>
2018-09-28 17:45:15 -0300

    Se agrego detalles portfolio

--- src/app/detalle-portfolio/detalle-portfolio.component.ts
+++ src/app/detalle-portfolio/detalle-portfolio.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
 
 @Component({
-  selector: 'app-detalle-portfolio',
-  templateUrl: './detalle-portfolio.component.html',
-  styleUrls: ['./detalle-portfolio.component.css']
+  selector: "app-detalle-portfolio",
+  templateUrl: "./detalle-portfolio.component.html",
+  styleUrls: ["./detalle-portfolio.component.css"]
 })
-export class DetallePortfolioComponent implements OnInit {
+export class DetallePortfolioComponent {
+ 
 
-  constructor() { }
+  
+  detalle = {
+    id: "bs-as",
+    titulo:"Portfolio Works!",
+    subTitulo: "Detalle portfolio...",
+    descripcion: "Bunos Aires, etc...",
+    fotos: [{ url: "/assets/bs-as.jpg", titulo:"foto1"},
+    { url: "/assets/bs-as.jpg", titulo:"foto2" },
+    { url: "/assets/icbc1.png", titulo:"foto3" },
+    { url: "/assets/bs-as.jpg", titulo:"foto4" }
+    
+  ]
+  };
 
-  ngOnInit() {
+
+  fotoActual = this.detalle.fotos[0];
+
+  mostrarFoto(element){
+    this.fotoActual = element;
   }
-
+  // detalleActual$ = this.router.root.paramMap.
 }
